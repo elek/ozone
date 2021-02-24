4bef87de7edb55f9a9a350b3f2078ccc0344092d
Elek Márton <elek@apache.org>
2021-02-24 13:20:39 +0100

    fix async future handling

--- hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/stream/DirstreamServerHandler.java
+++ hadoop-hdds/container-service/src/main/java/org/apache/hadoop/ozone/container/stream/DirstreamServerHandler.java
@@ -1,10 +1,13 @@
 package org.apache.hadoop.ozone.container.stream;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -18,6 +21,8 @@ import io.netty.util.ByteProcessor;
 public class DirstreamServerHandler extends ChannelInboundHandlerAdapter {
 
     private StreamingSource source;
+
+    private int writtenIndex = 0;
 
     private final StringBuilder id = new StringBuilder();
     private boolean headerProcessed = false;
@@ -43,35 +48,46 @@ public class DirstreamServerHandler extends ChannelInboundHandlerAdapter {
         }
 
         if (headerProcessed) {
-            ChannelFuture future = null;
+            ChannelFuture lastFuture = null;
 
-            for (Map.Entry<String, Path> entries : source
-                .getFilesToStream(id.toString())
-                .entrySet()) {
-                Path file = entries.getValue();
+            final List<Entry<String, Path>> entriesToWrite = new ArrayList<>(
+                source.getFilesToStream(id.toString()).entrySet());
 
-                String name = entries.getKey();
-                long fileSize = Files.size(file);
-                String identifier = fileSize + " " + name + "\n";
-                ByteBuf identifierBuf =
-                    Unpooled.wrappedBuffer(identifier.getBytes(
-                        StandardCharsets.UTF_8));
+            writeOneElement(ctx, entriesToWrite, 0);
 
-                if (future == null) {
-                    future = ctx.writeAndFlush(identifierBuf)
-                        .addListener(f -> ctx.writeAndFlush(
-                            new DefaultFileRegion(file.toFile(), 0, fileSize)));
-
-                } else {
-                    future = future.addListener(f -> ctx.writeAndFlush(identifierBuf))
-                        .addListener(f -> ctx.writeAndFlush(
-                            new DefaultFileRegion(file.toFile(), 0, fileSize)));
-
-                }
-            }
-
-            future.addListener(f -> ctx.channel().close());
         }
+    }
+
+    public void writeOneElement(
+        ChannelHandlerContext ctx,
+        List<Entry<String, Path>> entriesToWrite,
+        int i
+    )
+        throws IOException {
+        final Entry<String, Path> entryToWrite = entriesToWrite.get(i);
+        Path file = entryToWrite.getValue();
+        String name = entryToWrite.getKey();
+        long fileSize = Files.size(file);
+        String identifier = fileSize + " " + name + "\n";
+        ByteBuf identifierBuf =
+            Unpooled.wrappedBuffer(identifier.getBytes(
+                StandardCharsets.UTF_8));
+
+        final int currentIndex = i;
+
+        ChannelFuture lastFuture = ctx.writeAndFlush(identifierBuf);
+        lastFuture.addListener(f -> {
+            ChannelFuture nextFuture = ctx.writeAndFlush(
+                new DefaultFileRegion(file.toFile(), 0, fileSize));
+            if (currentIndex == entriesToWrite.size() - 1) {
+                nextFuture.addListener(a -> ctx.channel().close());
+            } else {
+                nextFuture.addListener(
+                    a -> writeOneElement(ctx, entriesToWrite,
+                        currentIndex + 1));
+            }
+        });
+
     }
 
     @Override
