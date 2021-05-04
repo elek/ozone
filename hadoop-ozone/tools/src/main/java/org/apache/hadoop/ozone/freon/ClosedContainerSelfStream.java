93c9ce1abe128e79e1212fcbfc9c3eb8691c98dd
Elek Márton <elek@apache.org>
2021-05-04 12:37:56 +0200

    f

---
+++ hadoop-ozone/tools/src/main/java/org/apache/hadoop/ozone/freon/ClosedContainerSelfStream.java
@@ -0,0 +1,139 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership.  The ASF
+ * licenses this file to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.hadoop.ozone.freon;
+
+import com.codahale.metrics.Timer;
+import org.apache.hadoop.hdds.cli.HddsVersionProvider;
+import org.apache.hadoop.hdds.conf.ConfigurationSource;
+import org.apache.hadoop.hdds.conf.OzoneConfiguration;
+import org.apache.hadoop.hdds.protocol.DatanodeDetails;
+import org.apache.hadoop.hdds.protocol.proto.HddsProtos.LifeCycleState;
+import org.apache.hadoop.hdds.scm.cli.ContainerOperationClient;
+import org.apache.hadoop.hdds.scm.container.ContainerInfo;
+import org.apache.hadoop.hdds.scm.container.common.helpers.ContainerWithPipeline;
+import org.apache.hadoop.hdfs.server.datanode.StorageLocation;
+import org.apache.hadoop.ozone.container.common.impl.ContainerSet;
+import org.apache.hadoop.ozone.container.common.volume.MutableVolumeSet;
+import org.apache.hadoop.ozone.container.replication.*;
+import org.jetbrains.annotations.NotNull;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+
+/**
+ * Utility to replicated closed container with datanode code.
+ */
+@Command(name = "css",
+    aliases = "container-self-stream",
+    description = "Start a container replicator server and client and tries to stream as much data as possible",
+    versionProvider = HddsVersionProvider.class,
+    mixinStandardHelpOptions = true,
+    showDefaultValues = true)
+public class ClosedContainerSelfStream extends BaseFreonGenerator implements
+    Callable<Void> {
+
+  private ReplicationSupervisor supervisor;
+
+  private Timer timer;
+
+  private List<ReplicationTask> replicationTasks;
+
+  @Override
+  public Void call() throws Exception {
+
+    OzoneConfiguration conf = createOzoneConfiguration();
+
+    final Collection<String> datanodeStorageDirs =
+        MutableVolumeSet.getDatanodeStorageDirs(conf);
+
+    for (String dir : datanodeStorageDirs) {
+      checkDestinationDirectory(dir);
+    }
+
+    startReplicationServer();
+
+    init();
+
+    timer = getMetrics().timer("stream-container");
+    runTests(this::replicateContainer);
+    return null;
+  }
+
+  private void startReplicationServer() {
+    ReplicationServer();
+  }
+
+  /**
+   * Check id target directory is not re-used.
+   */
+  private void checkDestinationDirectory(String dirUrl) throws IOException {
+    final StorageLocation storageLocation = StorageLocation.parse(dirUrl);
+    final Path dirPath = Paths.get(storageLocation.getUri().getPath());
+
+    if (Files.notExists(dirPath)) {
+      return;
+    }
+
+    if (Files.list(dirPath).count() == 0) {
+      return;
+    }
+
+    throw new IllegalArgumentException(
+        "Configured storage directory " + dirUrl
+            + " (used as destination) should be empty");
+  }
+
+  @NotNull
+  private void initializeReplicationSupervisor(ConfigurationSource conf)
+      throws IOException {
+    String fakeDatanodeUuid = UUID.randomUUID().toString();
+
+    ContainerSet containerSet = new ContainerSet();
+
+    MutableVolumeSet volumeSet = new MutableVolumeSet(fakeDatanodeUuid, conf);
+
+    final UUID scmId = UUID.randomUUID();
+    ContainerReplicator replicator;
+
+    replicator = new DownloadAndDiscardReplicator(
+        conf,
+        () -> scmId.toString(),
+        containerSet,
+        new NullContainerDownloader(conf, null),
+        volumeSet);
+    supervisor = new ReplicationSupervisor(containerSet, replicator, 10);
+  }
+
+  private void replicateContainer(long counter) throws Exception {
+    timer.time(() -> {
+      final ReplicationTask replicationTask =
+          replicationTasks.get((int) counter);
+      supervisor.new TaskRunner(replicationTask).run();
+      return null;
+    });
+  }
+}
